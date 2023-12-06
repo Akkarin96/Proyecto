0f6ab0fddc6a296760e23f31b7c264f3502129b0
Akkarin96 <150980048+Akkarin96@users.noreply.github.com>
2023-12-06 17:38:41 -0600

    Modificaciones para uso
    implementacion de las nuevas clases en el menu para su uso en el main padron

--- src/main/java/com/mycompany/padron/Padron.java
+++ src/main/java/com/mycompany/padron/Padron.java
@@ -9,6 +9,7 @@ import Classes.CrearArreglo;
 import Classes.Votos;
 import Intermedio.Menu;
 import java.io.IOException;
+import java.text.ParseException;
 
 
 /**
@@ -17,9 +18,31 @@ import java.io.IOException;
  */
 public class Padron {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         Menu menu=new Menu();
         menu.menuInicio();
-       
+        //Working Main
+       /* String padronData="";
+        String partidosData="";
+        short filasPadron=0;
+        short columnasPadron=0;
+        short filasPartidos=0;
+        short columnasPartidos=0;
+        FileChooser fc= new FileChooser();
+        String padronPath=fc.getFilePath();
+        String partidosPath=fc.getFilePath();
+        FileReader fr= new FileReader(padronPath,partidosPath);
+        fr.dataLoad();
+        padronData=fr.getFileData();
+        partidosData=fr.getPartidosData();
+        ArrayCreator arrays= new ArrayCreator(padronData,partidosData);
+        arrays.crearArrays();
+        filasPartidos=arrays.getFilasPartidos();
+        columnasPartidos=arrays.getColumnasPartidos();
+        filasPadron=arrays.getFilasPadron();
+        columnasPadron=arrays.getColumnasPadron();
+        Votos votos=new Votos(filasPadron,columnasPadron,filasPartidos,columnasPartidos,padronData,partidosData);
+        votos.Votaciones();
+        */
     }
 }
--- src/main/java/intermedio/Menu.java
+++ src/main/java/intermedio/Menu.java
@@ -4,27 +4,54 @@
  */
 package Intermedio;
 
+import Classes.CrearArreglo;
+import Classes.Ayuda;
+import Classes.Consultas;
 import Classes.FileChooser;
 import Classes.FileReader;
+import Classes.Votos;
 import java.io.IOException;
+import java.text.ParseException;
 import javax.swing.JOptionPane;
-
+import org.apache.commons.lang3.ArrayUtils;
 /**
  *
  * @author alba_
  */
 public class Menu {
     
-    public void menuInicio() throws IOException{
+    public String[][] getPadron() {
+        return padron;
+    }
+
+    public void setPadron(String[][] padron) {
+        this.padron = padron;
+    }
+
+    public String[][] getPartidos() {
+        return partidos;
+    }
+
+    public void setPartidos(String[][] partidos) {
+        this.partidos = partidos;
+    }
+       String[][] padron;
+       String[][] partidos;
+    
+    public void menuInicio() throws IOException, ParseException{
+
+       String datosPadron="";
+       String datosPartidos="";
        int seleccion=0;
-       while (seleccion<4) {
+       while (seleccion<5) {
          do{   
-         seleccion=Integer.parseInt(JOptionPane.showInputDialog(null,"Seleccione el numero de la opcion deseada"+"\n"+
+         seleccion=Integer.parseInt(JOptionPane.showInputDialog(null,"Select any of the following options:"+"\n"+
                  "1- Iniciar Votaciones"+"\n"+
                  "2- Realizar Consultas"+"\n"+
                  "3- Imprimir Resultados de Votacion"+"\n"+
-                 "4- Salir"));
-         }while (seleccion>4);
+                 "4- Ayuda"+"\n"+
+                 "5- Salir"));
+         }while (seleccion>5);
          switch (seleccion){
              case 1:
                  FileChooser seleccionArchivos=new FileChooser();
@@ -34,19 +61,74 @@ public class Menu {
                  String archivoPartidos=seleccionArchivos.getFilePath();
                  FileReader lectorArchivos= new FileReader(archivoPadron, archivoPartidos);
                  lectorArchivos.dataLoad();
+                 datosPadron=lectorArchivos.getFileData();
+                 datosPartidos=lectorArchivos.getPartidosData();
+                 CrearArreglo crearArrays=new CrearArreglo(datosPadron, datosPartidos);
+                 crearArrays.crearArrays();
+                 setPadron(crearArrays.getPadron());
+                 setPartidos(crearArrays.getPartidos());
+                 Votos votos= new Votos(getPadron(),getPartidos());
+                 votos.Votaciones();
+                 setPadron(votos.getPadron());
+                 setPartidos(votos.getPartidos());
                  break;
-                 
              case 2:
-                 
+                 int tipoConsulta=0;
+                 if((ArrayUtils.isEmpty(padron))||ArrayUtils.isEmpty(partidos)){
+                     JOptionPane.showMessageDialog(null, "Para realizar consultas, debe llenar los padrones primero!");
+                     break;
+                 }
+                 else{
+                    do{   
+                        tipoConsulta=Integer.parseInt(JOptionPane.showInputDialog(null,"Select any of the following options:"+"\n"+
+                        "1- Consulta por Nombres"+"\n"+
+                        "2- Consulta por Primer Apellido"+"\n"+
+                        "3- Consulta por Segundo Apellido"+"\n"+
+                        "4- Volver al Menu Principal"));
+                    }while (tipoConsulta>4);
+                    Consultas cons=new Consultas(getPadron(),getPartidos());
+                    String nombreBuscar="";
+                    switch (tipoConsulta){
+                        case 1:                    
+                            nombreBuscar=JOptionPane.showInputDialog("Digite el nombre a consultar:");
+                            cons.consultasNombre(nombreBuscar);
+                            break;
+                        case 2:
+                            nombreBuscar=JOptionPane.showInputDialog("Digite el apellido a consultar:");
+                            cons.consultasApellido1(nombreBuscar);
+                        break; 
+                        case 3: 
+                            nombreBuscar=JOptionPane.showInputDialog("Digite el apellido a consultar:");
+                            cons.consultasApellido2(nombreBuscar);
+                            break;
+                        case 4: 
+                            break;
+                        default: JOptionPane.showMessageDialog(null, "Opcion Invalida. Intente de nuevo");      
+                    }
+                    break; 
+                    }
              case 3:
-                
+                 if((ArrayUtils.isEmpty(padron))||ArrayUtils.isEmpty(partidos)){
+                     JOptionPane.showMessageDialog(null, "Para realizar consultas, debe llenar los padrones primero!");
+                     break;
+                 }
+                 else{
+                    String msg="";
+                    int nulos=0;
+                    Consultas res=new Consultas(getPadron(),getPartidos());
+                    msg=res.ganadorOficial()+"\n"+"Total de votos nulos: "+res.obtenerVotosNulos();
+                    JOptionPane.showMessageDialog(null, msg); 
+                    break;
+                 }
              case 4:
-                 JOptionPane.showMessageDialog(null, "Gracias por usar el programa :D ");
+                 Ayuda ayuda=new Ayuda();
+                 ayuda.mensajeAyuda();
+                 break;
+             case 5:
+                 JOptionPane.showMessageDialog(null, "Thanks for using our system");
                  break;
              default:
-                 JOptionPane.showMessageDialog(null, "Opcion invalida, intente de nuevo >:v ");
-
-                         
+                 JOptionPane.showMessageDialog(null, "Invalid Option, Try again");                
          }
         }
     }
